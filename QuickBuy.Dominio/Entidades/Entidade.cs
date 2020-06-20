275bdaa6f2e61d4b712e204e303834b5b8293613
Adielson Junior <32928428+adielsonjunior@users.noreply.github.com>
2020-06-20 18:03:56 -0300

    Classe de Validação

--- QuickBuy.Dominio/Entidades/Entidade.cs
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -1,15 +1,33 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
 
 namespace QuickBuy.Dominio.Entidades
 {
     public abstract class Entidade
     {
+        public List<string> _mensagensValidacao { get; set; }
+        private List<string> mensagemValidacao
+        {
+            get { return _mensagensValidacao ?? (_mensagensValidacao = new List<string>()); }
+        }
 
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
-        public string Nome { get; set; }
-        public string SobreNome { get; set; }
+        public abstract void Validate();
+
+        protected bool EhValido {
+            get { return !mensagemValidacao.Any(); }
+        }
+
+        protected void AdicionarCritica(string mensagem) {
+            mensagemValidacao.Add(mensagem);
+        
+        }
+
+        protected void LimparmensagensValidacao() {
+            mensagemValidacao.Clear();
+        }
+
 
     }
 }
--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,11 +1,12 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
 {
-   public  class Pedido
+   public  class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -16,8 +17,20 @@ namespace QuickBuy.Dominio.Entidades
         public string Cidade { get; set; }
         public string EnderecoCompleto { get; set; }
         public string NumeroEndereco { get; set; }
-        public ICollection<ItemPedido> ItemPedido { get; set; }
+        public ICollection<ItemPedido> ItensPedido { get; set; }
         public int FormaPagamentoId { get; set; }
         public FormaPagamento  FormaPagamento { get; set; }
+
+        public override void Validate()
+        {
+            LimparmensagensValidacao();
+            if (!ItensPedido.Any()) {              
+                AdicionarCritica("Crítica - Pedido não pode ficar sem item de pedido.");            
+            }
+            if (string.IsNullOrEmpty(Cep))
+            {
+                AdicionarCritica("Crítica - CEP não foi preenchido");
+            }
+        }
     }
 }
