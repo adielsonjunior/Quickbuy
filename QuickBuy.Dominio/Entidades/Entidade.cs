580f0ded81236aa5bd81d948dbf83d3490b3eb43
Adielson Junior <32928428+adielsonjunior@users.noreply.github.com>
2020-06-20 17:38:42 -0300

    Configurações Endidades

---
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public abstract class Entidade
+    {
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+    }
+}
---
+++ QuickBuy.Dominio/Enumeradores/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,12 @@
+﻿
+namespace QuickBuy.Dominio.Enumeradores
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefindo = 0,
+        Boleto =1,
+        CartaoCredito =2,
+        Deposito = 3
+
+    }
+}
