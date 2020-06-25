585b92a72db11234ff8e8299bba57044dfe2037b
Adielson Junior <32928428+adielsonjunior@users.noreply.github.com>
2020-06-25 19:03:32 -0300

    Versao 1.0 da api

--- QuickBuy.Repositorio/Repositorio/ProdutoRepositorio.cs
+++ QuickBuy.Repositorio/Repositorio/ProdutoRepositorio.cs
@@ -1,5 +1,6 @@
 ﻿using QuickBuy.Dominio.Contratos;
 using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Contexto;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,10 @@ using System.Text;
 namespace QuickBuy.Repositorio.Repositorio
 {
     public class ProdutoRepositorio: BaseRepositorio<Produto>,IProdutoRepositorio
-    {
+    {         
+        public ProdutoRepositorio(QuickBuyContexto quickBuyContexto):base(quickBuyContexto)
+        {
+
+        }
     }
 }
